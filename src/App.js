10954ff2c090904e7ba65812dda7322c1fe6a9b8
jehutty <athsivridis@gmail.com>
2020-11-29 23:24:27 +0000

    removed eslintcache from repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { MainComponent } from './components/MainComponent';
+import { TodoPomodoro } from './components/TodoPomodoro';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route path={'/'} component={MainComponent}></Route>
+          <Route path={'/pomodo'} component={TodoPomodoro}></Route>
         </Switch>
       </Router>
     </>
